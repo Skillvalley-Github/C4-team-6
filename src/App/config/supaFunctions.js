90e03d53946f1309389f5dbdd261f06374468b2f
Rishabh Dubey <rishabh.dubey.cs21@ggits.net>
2023-09-10 20:21:43 +0530

    get by id made

--- src/App/config/supaFunctions.js
+++ src/App/config/supaFunctions.js
@@ -6,3 +6,14 @@ export const getAlumini = async () => {
   if (error) return error;
   return alumini;
 };
+
+//get alumni by id
+//here id is the key of the alumni table in supabase since it is the primary key
+export const getAlumniById = async (key) => {
+  let { data: alumni, error } = await supabase
+    .from(TableNames.alumni)
+    .select("*")
+    .eq("key", key);
+  if (error) return error;
+  return alumni;
+};
