0f09f60404aa88eaf4513033265144f7baa3915a
Swastik Bose <98341839+swastik-bose007@users.noreply.github.com>
2023-09-27 23:07:19 +0530

    implemented alumni search

--- src/App/ScreenComponents/AlumnusComponent/Alumnus.js
+++ src/App/ScreenComponents/AlumnusComponent/Alumnus.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import CarouselCard from "./components/CarouselCard";
 import { CiSearch } from "react-icons/ci";
 import AllSeniors from "./components/AllSeniors";
-import { getAlumini,getAlumniById } from "../../Config/supaFunctions";
+import { getAlumini, getAlumniById } from "../../Config/supaFunctions";
 import { useQuery } from "react-query";
 
 const Alumnus = () => {
-  const { data: alumini, isLoading, isError } = useQuery(["alumini"], getAlumini);
+  const {
+    data: alumini,
+    isLoading,
+    isError,
+  } = useQuery(["alumini"], getAlumini);
+
+  const [searchInput, setSearchInput] = useState("");
 
   if (isLoading) {
     return <p>Loading...</p>; // You can replace this with a loading spinner or component
   }
 
   // console.log(alumini);
+
+  const filteredAlumni = alumini.filter((alumni) =>
+    alumni.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <>
@@ -37,11 +47,15 @@ const Alumnus = () => {
               type="text"
               className="px-16 py-4 md:py-3.5 mt-2 md:mt-0 border-dashed border-2 rounded-full bg-color-4"
               placeholder="Search alumni from feed"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
             />
           </div>
         </div>
         <div className="container md:col-span-1">
-          <AllSeniors AlumniDataProps={alumini} />
+          <AllSeniors
+            AlumniDataProps={searchInput ? filteredAlumni : alumini}
+          />
         </div>
       </div>
     </>
